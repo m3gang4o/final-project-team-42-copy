6a0d174ed736a539c0da7e6d0a0c384fbdf05105
Megan <gaom@unc.edu>
2025-12-14 17:59:17 -0500

    Type fix

--- components/group-chat.tsx
+++ components/group-chat.tsx
@@ -243,12 +243,12 @@ export function GroupChat({ group, user, authorId }: GroupChatProps) {
     });
 
     // Join event
-    channel.on("presence", { event: "join" }, ({ key }) => {
+    channel.on("presence", { event: "join" }, ({ key }: { key: string }) => {
       setOnlineMembers((prev) => (prev.includes(key) ? prev : [...prev, key]));
     });
 
     // Leave event
-    channel.on("presence", { event: "leave" }, ({ key }) => {
+    channel.on("presence", { event: "leave" }, ({ key }: { key: string }) => {
       setOnlineMembers((prev) => prev.filter((id) => id !== key));
     });
 
